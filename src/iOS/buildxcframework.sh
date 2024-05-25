a8157a3230c5701f1b0a68294c9d33c7af9e0605
Brian Runck <brunck@intermatic.com>
2024-05-24 22:55:00 -0400

    Copy Swift header files for Objective Sharpie; add error handling for Carthage command; some other cleanup

--- src/iOS/buildxcframework.sh
+++ src/iOS/buildxcframework.sh
@@ -12,7 +12,11 @@ CARTHAGE_OUTPUT="${CARTFILE_DIRECTORY}/Carthage/Build"
 
 echo
 echo "Building XCFrameworks for Datadog SDK."
-carthage update --platform iOS --use-xcframeworks --project-directory "$CARTFILE_DIRECTORY"
+carthage update --platform iOS --use-xcframeworks --project-directory "${CARTFILE_DIRECTORY}"
+if [ $? -ne 0 ]; then
+  echo "Carthage update failed. Exiting."
+  exit 1
+fi
 
 # Define output folder environment variable
 OUTPUT_FOLDER="${PWD}/build"
@@ -37,7 +41,7 @@ for FRAMEWORK_NAME in "${FRAMEWORK_NAMES[@]}"; do
   echo
   if ! xcodebuild -workspace "${WORKSPACE}" archive \
     -scheme "${SCHEME}" \
-    -archivePath ${SIMULATOR_ARCHIVE_PATH} \
+    -archivePath "${SIMULATOR_ARCHIVE_PATH}" \
     -destination "generic/platform=iOS Simulator" \
     -derivedDataPath "${DERIVED_DATA_PATH}" \
     -IDECustomBuildProductsPath="" -IDECustomBuildIntermediatesPath="" \
@@ -54,7 +58,7 @@ for FRAMEWORK_NAME in "${FRAMEWORK_NAMES[@]}"; do
   echo
   if ! xcodebuild -workspace "${WORKSPACE}" archive \
     -scheme "${SCHEME}" \
-    -archivePath ${DEVICE_ARCHIVE_PATH} \
+    -archivePath "${DEVICE_ARCHIVE_PATH}" \
     -destination "generic/platform=iOS" \
     -derivedDataPath "${DERIVED_DATA_PATH}" \
     -IDECustomBuildProductsPath="" -IDECustomBuildIntermediatesPath="" \
@@ -70,9 +74,9 @@ for FRAMEWORK_NAME in "${FRAMEWORK_NAMES[@]}"; do
   echo "Creating XCFramework for ${FRAMEWORK_NAME}."
   echo
   if ! xcodebuild -create-xcframework \
-    -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-    -framework ${DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-    -output ${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework; then
+    -framework "${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -framework "${DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -output "${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework"; then
       echo "Create XCFramework for ${FRAMEWORK_NAME} failed. Exiting."
       echo
       exit 1
@@ -116,9 +120,28 @@ echo "Copying CrashReporter.xcframework to target directory ${TARGET_DIR}."
 echo
 cp -R "${CARTHAGE_OUTPUT}/CrashReporter.xcframework" "${TARGET_DIR}"
 if [ $? -ne 0 ]; then
-  echo "Failed to copy ${XCFRAMEWORK_NAME}. Exiting."
+  echo "Failed to copy CrashReporter.xcframework. Exiting."
   exit 1
 fi
+
+HEADER_FILE_PREFIXES=("DatadogObjc" "DatadogCrashReporting" "DatadogInternal" "DatadogSessionReplay")
+
+echo "Copying Swift header files to target directory ${TARGET_DIR} for Objective Sharpie."
+
+for FRAMEWORK_NAME in "${HEADER_FILE_PREFIXES[@]}"; do
+  XCFRAMEWORK_PATH="${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework"
+  HEADER_FILE_PATH=$(find "${XCFRAMEWORK_PATH}" -name "${FRAMEWORK_NAME}-Swift.h" | head -n 1)
+  if [ -z "${HEADER_FILE_PATH}" ]; then
+
+    echo "Failed to find ${FRAMEWORK_NAME}-Swift.h in ${XCFRAMEWORK_PATH}. Exiting."
+    exit 1
+  fi
+  cp -f "${HEADER_FILE_PATH}" "${TARGET_DIR}"
+  if [ $? -ne 0 ]; then
+    echo "Failed to copy ${HEADER_FILE_PATH} to ${TARGET_DIR}. Exiting."
+    exit 1
+  fi
+done
 
 echo
 echo "Done."
