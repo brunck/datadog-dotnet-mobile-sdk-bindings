a09a1201cfb5e6d429235ca05e0c9bf28f59a22b
Brian Runck <brunck@intermatic.com>
2024-06-28 19:44:02 -0400

    Rum in progress

---
+++ src/Android/Bindings/Rum/Additions/RumResourceKind.cs
@@ -0,0 +1,11 @@
+﻿namespace Datadog.Android.Rum
+{
+    public partial class RumResourceKind
+    {
+        public static RumResourceKind FromMimeType(string mimeType)
+        {
+            var companion = new Companion(null);
+            return companion.FromMimeType(mimeType);
+        }
+    }
+}
