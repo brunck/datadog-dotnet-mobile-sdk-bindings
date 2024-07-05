4c4b3df979fcb5b8fb706cc011545e49633c476f
Brian Runck <brian.c.runck@gmail.com>
2024-07-05 16:49:33 -0400

    Added DDWebViewTracking

--- src/iOS/Bindings/ApiDefinitions.cs
+++ src/iOS/Bindings/ApiDefinitions.cs
@@ -2,6 +2,7 @@ using System;
 using Foundation;
 using ObjCRuntime;
 using UIKit;
+using WebKit;
 
 namespace Datadog.iOS
 {
@@ -5621,6 +5622,22 @@ namespace Datadog.iOS
 		[Static]
 		[Export ("enable")]
 		void Enable ();
+	}
+
+	// @interface DDWebViewTracking : NSObject
+	[BaseType (typeof(NSObject))]
+	[DisableDefaultCtor]
+	interface DDWebViewTracking
+	{
+		// +(void)enableWithWebView:(WKWebView * _Nonnull)webView hosts:(NSSet<NSString *> * _Nonnull)hosts logsSampleRate:(float)logsSampleRate;
+		[Static]
+		[Export ("enableWithWebView:hosts:logsSampleRate:")]
+		void Enable (WKWebView webView, NSSet<NSString> hosts, float logsSampleRate);
+
+		// +(void)disableWithWebView:(WKWebView * _Nonnull)webView;
+		[Static]
+		[Export ("disableWithWebView:")]
+		void Disable (WKWebView webView);
 	}
 
 	// @interface DatadogURLSessionDelegate : NSObject <NSURLSessionDataDelegate>
