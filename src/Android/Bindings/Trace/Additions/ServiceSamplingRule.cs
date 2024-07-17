f64f0588523e3a58cc86fc3427e3f80e776c8307
Brian Runck <brunck@intermatic.com>
2024-07-17 17:01:42 -0400

    Added Trace project

---
+++ src/Android/Bindings/Trace/Additions/ServiceSamplingRule.cs
@@ -0,0 +1,13 @@
+﻿namespace Datadog.Android.Trace.Common.Sampling
+{
+    public partial class SamplingRule
+    {
+        public partial class ServiceSamplingRule
+        {
+            protected override Java.Lang.ICharSequence? GetRelevantStringFormatted(Java.Lang.Object? span)
+            {
+                return (Java.Lang.String?)GetRelevantString(span);
+            }
+        }
+    }
+}
