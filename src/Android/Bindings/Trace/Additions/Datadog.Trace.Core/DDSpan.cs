1ba7b905776bb19ea590f2b7dff5ef37985d3f16
Brian Runck <brian.c.runck@gmail.com>
2024-07-21 23:50:01 -0400

    Trace in progress

---
+++ src/Android/Bindings/Trace/Additions/Datadog.Trace.Core/DDSpan.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Datadog.Trace.Core;
+
+// ReSharper disable once InconsistentNaming
+public partial class DDSpan
+{
+    public int SamplingPriority()
+    {
+        return (int)SamplingPriorityInternal()!;
+    }
+
+    
+}
