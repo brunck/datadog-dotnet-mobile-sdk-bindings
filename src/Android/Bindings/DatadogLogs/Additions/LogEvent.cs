85967f9025c9d5669d07aa6a4bce9f9a4d71e36c
Brian Runck <brunck@intermatic.com>
2024-08-29 20:10:49 -0400

    Additions to Logs and Core

---
+++ src/Android/Bindings/DatadogLogs/Additions/LogEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace Datadog.Android.Log.Model;
+
+public partial class LogEvent
+{
+    public Error? ErrorDetails => GetError();
+
+    public Logger LoggerDetails => GetLogger();
+
+    public Network? NetworkDetails => GetNetwork();
+
+    public Status StatusDetails => GetStatus();
+
+    public DatadogDeviceInfo DD => DDInternal();
+
+    public string DDTags
+    {
+        get => DdTagsGet();
+        set => SetDdtags(value);
+    }
+}
+
+
---
+++ src/Android/Bindings/DatadogLogs/Additions/LogsConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Datadog.Android.Event;
+using Datadog.Android.Log.Model;
+
+namespace Datadog.Android.Log;
+
+public partial class LogsConfiguration
+{
+    public partial class Builder
+    {
+        public Builder SetEventMapper(IEventMapper<LogEvent> eventMapper)
+        {
+            return SetLogEventMapperInternal(eventMapper);
+        }
+    }
+}
