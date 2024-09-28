60c088428b7b96d65a7e2f01332263f544e1f856
Brian Runck <brian.c.runck@gmail.com>
2024-09-28 17:57:34 -0400

    update mdoc script

--- src/iOS/Bindings/buildmdoc.sh
+++ src/iOS/Bindings/buildmdoc.sh
@@ -3,7 +3,7 @@
 # Define the library path and output directory
 LIBRARY_PATH="/usr/local/share/dotnet/packs/Microsoft.iOS.Ref/17.2.8053/ref/net8.0"
 OUTPUT_DIR="Mdoc"
-DLL_PATH="/Users/brian/Documents/SourceCode/datadog-dotnet-mobile-sdk-bindings/src/iOS/Bindings/bin/Debug/net8.0-ios/ios.dll"
+DLL_PATH="/Users/brian/Documents/SourceCode/datadog-dotnet-mobile-sdk-bindings/src/iOS/Bindings/bin/Release/net8.0-ios/ios.dll"
 
 # Execute the mdoc update command with sudo
 sudo mdoc update -L "$LIBRARY_PATH" --out "$OUTPUT_DIR" "$DLL_PATH" --debug
